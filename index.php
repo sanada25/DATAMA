dc576c3596b4dd4ce89d4b80489f2fb98bda437d
JamesTabulao <117695490+JamesTabulao@users.noreply.github.com>
2023-02-06 13:27:39 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+    <form action="login.php" method="post">
+     	<h1>LOGIN FORM</h1>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     
+     	<input type="text" name="uname" placeholder="Username"><br>
+
+     	
+     	<input type="password" name="password" placeholder="Password"><br>
+
+     	<button type="submit">Login</button>
+
+     </form>
+       
+</body>
+</html>
